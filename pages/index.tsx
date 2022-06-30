ca81b66b39a03981cb8b0f7eefa8393ec150cf6c
Shriram Balaji <hello@shrirambalaji.dev>
2022-06-30 23:40:23 +0530

    refactor(page): update homepage with name, description and job title sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,32 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import { Avatar } from "../components/Avatar";
+import { MicrosoftLogo } from "../components/icons";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <main className="flex flex-col justify-center mx-auto max-w-3xl">
+      <div className="flex flex-col sm:flex-row items-start ml-4">
+        <section className="flex flex-col pr-8 gap-2.5 w-full">
+          <h1 className="font-bold text-3xl md:text-5xl ml-[-0.25rem] text-ghostindigo-900 dark:text-white">
+            Shriram Balaji
+          </h1>
+          <h2 className="md:text-xl text-gray-800 dark:text-gray-300 tracking-wide inline-flex items-center">
+            Software Engineer II at
+            <span className="ml-2 font-bold inline-flex items-center gap-1.5 text-gray-800 dark:text-gray-300 tracking-wide">
+              <MicrosoftLogo />
+            </span>
+          </h2>
+          <p className="text-gray-500 dark:text-gray-500 tracking-wide">
+            Dabbling with things on the interwebs and striving for software
+            craftsmanship.
+          </p>
+        </section>
+        <section className="ml-auto z-10">
+          <Avatar className="dark:shadow-3xl" />
+        </section>
+      </div>
+    </main>
+  );
+};
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
