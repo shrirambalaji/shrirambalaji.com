029776393bf3090d551db414aa39a9a17fa45861
Shriram Balaji <hello@shrirambalaji.dev>
2022-06-30 23:38:10 +0530

    feat(styling): update navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,9 +17,9 @@ const NavItem = ({ href, text }: NavItemProps) => {
       <a
         className={cn(
           isActive
-            ? "font-semibold text-gray-800 dark:text-gray-200"
-            : "font-normal text-gray-600 dark:text-gray-400",
-          "hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-ghostindigo-50 dark:hover:bg-ghostindigo-800 transition-all"
+            ? "font-semibold text-ghostindigo-700 dark:text-gray-200 "
+            : "font-normal text-ghostindigo-500 dark:text-gray-300",
+          "hidden md:inline-block p-1 sm:px-2 sm:py-1.5 rounded-lg hover:backdrop-blur-xl hover:bg-ghostindigo-50 dark:hover:bg-ghostindigo-800 transition-all w-20 text-center"
         )}
       >
         <span className="capsize">{text}</span>
@@ -30,12 +30,12 @@ const NavItem = ({ href, text }: NavItemProps) => {
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16 text-gray-900 dark:bg-ghostindigo-900 bg-opacity-60 dark:text-gray-100">
+    <nav className="flex items-center justify-start w-full relative max-w-3xl mx-u border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16 text-gray-900 dark:bg-ghostindigo-900 bg-opacity-60 dark:text-gray-100">
       <NavItem href="/" text="Home" />
       <NavItem href="/about" text="About" />
       <NavItem href="/uses" text="Uses" />
-      <NavItem href="/blog" text="Blog" />
-      <ThemeSwitch />
+      <NavItem href="https://blog.shrirambalaji.dev" text="Blog" />
+      <ThemeSwitch className="ml-auto mr-[3.4rem]" />
     </nav>
   );
 };
