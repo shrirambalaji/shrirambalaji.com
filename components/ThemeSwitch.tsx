ba3952b77311ecff4e108107f20e8247c76d00cf
Shriram Balaji <hello@shrirambalaji.dev>
2022-06-30 23:38:28 +0530

    refactor(components): move icons out of theme switcher

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,47 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, PropsWithChildren } from "react";
 import { useTheme } from "next-themes";
+import cn from "classnames";
+import { DarkMode, LightMode } from "./icons";
 
-const DarkModeIcon = () => {
-  return (
-    <svg
-      fill="none"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-      />
-    </svg>
-  );
-};
-
-const LightModeIcon = () => {
-  return (
-    <svg
-      fill="none"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-      />
-    </svg>
-  );
-};
-
-export const ThemeSwitch = () => {
+export const ThemeSwitch = (
+  props: PropsWithChildren<{ className?: string }>
+) => {
   const { theme, setTheme, resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const { className } = props;
 
   useEffect(() => setMounted(true), []);
 
@@ -50,19 +17,22 @@ export const ThemeSwitch = () => {
   };
 
   const isDarkMode = mounted && (theme === "dark" || resolvedTheme === "dark");
- 
+
   return (
     <span
       role="button"
       aria-label="Toggle Dark Mode"
-      className="text-current p-2 cursor-pointer ml-3"
+      className={cn(
+        "text-current p-2 cursor-pointer hover:dark:bg-ghostindigo-800 hover:bg-ghostindigo-50 rounded-lg text-center",
+        className
+      )}
       tabIndex={0}
       onClick={toggleTheme}
       onKeyDown={(e) => {
         if (e.key === "Enter") toggleTheme();
       }}
     >
-      {isDarkMode ? <DarkModeIcon /> : <LightModeIcon />}
+      {isDarkMode ? <LightMode /> : <DarkMode />}
     </span>
   );
 };
