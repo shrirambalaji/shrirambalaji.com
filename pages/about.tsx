e53754b61431a7848e626f47f8b82ecb04bd90f3
Shriram Balaji <hello@shrirambalaji.dev>
2022-06-30 23:41:08 +0530

    chore(page): init /about, /uses pages

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return <h1>/about</h1>;
+};
+
+export default About;
---
+++ pages/uses.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Uses: NextPage = () => {
+  return <h1>/uses</h1>;
+};
+
+export default Uses;
