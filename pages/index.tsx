3d59093a5d9d53b5ff0fdbfb2669049a111866cc
Shriram Balaji <hello@shrirambalaji.dev>
2022-07-01 21:49:10 +0530

    fix(styling): fix profile picture styles in mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { MicrosoftLogo } from "../components/icons";
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col justify-center mx-auto max-w-3xl">
-      <div className="flex flex-col sm:flex-row items-start ml-4">
+      <div className="flex flex-col-reverse sm:flex-row items-start ml-4">
         <section className="flex flex-col pr-8 gap-2.5 w-full">
           <h1 className="font-bold text-3xl md:text-5xl ml-[-0.25rem] text-ghostindigo-900 dark:text-white">
             Shriram Balaji
@@ -21,8 +21,8 @@ const Home: NextPage = () => {
             craftsmanship.
           </p>
         </section>
-        <section className="ml-auto z-10">
-          <Avatar className="dark:shadow-3xl" />
+        <section className="my-7 md:my-0 z-10">
+          <Avatar className="dark:shadow-3xl md:w-[180px] w-[120px]" />
         </section>
       </div>
     </main>
