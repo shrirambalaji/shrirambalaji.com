a73884b1370a098b148ac503c242a24035258b5e
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-24 01:38:21 +0530

    feat(page): add /uses page

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -1,7 +1,18 @@
-import { NextPage } from "next";
+import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
+import { PageContainer } from "../components/PageContainer";
+import { readMDXFile } from "../util/mdx";
 
-const Uses: NextPage = () => {
-  return <h1>/uses</h1>;
-};
+export default function Uses({ ...props }: MDXRemoteProps) {
+  return (
+    <PageContainer>
+      <div className="prose prose-slate prose-h1:text-[1.25rem] prose-h1:font-bold prose-h1:text-black prose-a:text-gray-600  dark:prose-invert dark:prose-h1:text-ghostindigo-300 dark:prose-a:text-indigo-300">
+        <MDXRemote compiledSource={props.compiledSource} />
+      </div>
+    </PageContainer>
+  );
+}
 
-export default Uses;
+export async function getStaticProps() {
+  const { compiledSource } = await readMDXFile("src/content/uses.mdx");
+  return { props: { compiledSource } };
+}
