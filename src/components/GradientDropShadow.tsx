36ea40f73594eafa0ce2a87a5df12b0aca141e5d
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-25 20:09:40 +0530

    fix: hide gradient drop shadow in mobile

--- src/components/GradientDropShadow.tsx
+++ src/components/GradientDropShadow.tsx
@@ -1,5 +1,5 @@
 export const GradientDropShadow = () => {
   return (
-    <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-400 to-indigo-400  opacity-0 blur-md  transition  duration-300 group-hover:opacity-20 dark:from-pink-800 dark:to-indigo-800" />
+    <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-400 to-indigo-400  opacity-0 blur-md  transition  duration-300 group-hover:opacity-0 dark:from-pink-800 dark:to-indigo-800 md:group-hover:opacity-20" />
   );
 };
