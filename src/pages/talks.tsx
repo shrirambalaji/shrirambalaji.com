61c2e2cfa21bc6b356fdc3a055268f147d881f46
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-26 02:28:35 +0530

    feat(page): update /talks styles

--- src/pages/talks.tsx
+++ src/pages/talks.tsx
@@ -5,7 +5,7 @@ import { readMDXFile } from "../util/mdx";
 export default function Talks({ ...props }: MDXRemoteProps) {
   return (
     <PageContainer>
-      <div className="prose prose-slate prose-h2:text-[1.5rem] prose-h2:font-bold prose-h2:tracking-normal prose-h2:text-black  prose-a:text-ghostindigo-400  dark:prose-invert dark:prose-h2:text-indigo-200 dark:prose-a:text-indigo-300">
+      <div className="prose prose-slate prose-h2:mb-1 prose-h2:font-bold prose-h2:leading-10 prose-h2:tracking-normal prose-h2:text-black prose-a:text-ghostindigo-400 dark:prose-invert  dark:prose-h2:text-indigo-200  dark:prose-a:text-indigo-300 prose-h2:md:mb-4 prose-h2:md:text-2xl">
         <MDXRemote compiledSource={props.compiledSource} />
       </div>
     </PageContainer>
