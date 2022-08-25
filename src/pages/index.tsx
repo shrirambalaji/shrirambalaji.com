39a70994f11c9e6623a9184a566fb2e06dbb0859
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-25 20:08:54 +0530

    refactor: move experienceList to separate component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,55 @@ type HomePageProps = {
   };
 };
 
+const ExperienceList = ({ work }: { work: Work[] }) => {
+  return (
+    <ul className="mb-3 flex w-full flex-col gap-5">
+      {work.map(
+        ({
+          company,
+          duration,
+          description,
+          link,
+          title,
+          image,
+          darkModeInvert,
+        }) => {
+          const size = 32;
+          return (
+            <li key={company} className="group relative cursor-pointer">
+              <GradientDropShadow />
+              <div className="relative flex flex-col items-center rounded-lg border bg-white px-3 py-1 shadow-sm dark:border-ghostindigo-800 dark:bg-ghostindigo-900 md:flex-row">
+                <Image
+                  className={`w-50 mx-4 mb-2 mt-5 self-start object-cover md:my-6 md:mx-2 ${
+                    darkModeInvert ? "dark:invert" : "filter-none"
+                  }`}
+                  src={image as string}
+                  width={size}
+                  height={size}
+                  alt={company}
+                />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <a href={link} className="underline-offset-2 hover:underline">
+                    <h5 className="text-2xl font-semibold text-ghostindigo-900 dark:text-white">
+                      {company}
+                    </h5>
+                  </a>
+                  <h6 className="text-md mb-2 text-ghostindigo-800 dark:text-ghostindigo-400 md:text-sm">
+                    {title}
+                  </h6>
+                  <p className="mb-3 font-normal leading-7 text-ghostindigo-700 dark:text-gray-400">
+                    {description}
+                  </p>
+                </div>
+              </div>
+            </li>
+          );
+        }
+      )}
+    </ul>
+  );
+};
+
 const Home: NextPage<HomePageProps> = (props) => {
   const { work, bio } = props;
   const currentJob = work[0];
@@ -34,13 +83,13 @@ const Home: NextPage<HomePageProps> = (props) => {
           <h1 className="ml-[-2px] text-3xl font-bold text-ghostindigo-900 dark:text-white md:text-5xl">
             {bio.name}
           </h1>
-          <h2 className="inline-flex items-center text-[16px] tracking-wide text-gray-800 dark:text-gray-300 md:text-xl">
+          <h2 className="inline-flex items-center text-[16px] text-xl tracking-wide text-gray-800 dark:text-gray-300">
             {currentJob.title} at
-            <span className="ml-2 inline-flex items-center gap-1.5 font-bold tracking-wide text-gray-800 dark:text-gray-300">
+            <span className="ml-2 inline-flex items-center gap-1.5 text-xl font-bold tracking-wide text-gray-800 dark:text-gray-300">
               {currentJob.company === "Microsoft" && <MicrosoftLogo />}
             </span>
           </h2>
-          <p className="tracking-wide text-gray-500 dark:text-gray-400">
+          <p className="text-lg tracking-wide text-gray-500 dark:text-gray-400 md:text-base">
             {bio.about}
           </p>
         </section>
@@ -52,53 +101,7 @@ const Home: NextPage<HomePageProps> = (props) => {
         <h3 className="mb-3 text-lg font-semibold uppercase tracking-widest dark:text-ghostindigo-400">
           Experience
         </h3>
-        <ul className="mb-3 flex w-full flex-col gap-5">
-          {work.map(
-            ({
-              company,
-              duration,
-              description,
-              link,
-              title,
-              image,
-              darkModeInvert,
-            }) => {
-              const size = 32;
-              return (
-                <li key={company} className="group relative cursor-pointer">
-                  <GradientDropShadow />
-                  <div className="relative flex flex-col items-center rounded-lg border bg-white px-3 py-1 shadow-sm dark:border-ghostindigo-800 dark:bg-ghostindigo-900 md:flex-row">
-                    <Image
-                      className={`w-50 mx-4 mb-2 mt-5 self-start object-cover md:my-6 md:mx-2 ${
-                        darkModeInvert ? "dark:invert" : "filter-none"
-                      }`}
-                      src={image as string}
-                      width={size}
-                      height={size}
-                      alt={company}
-                    />
-                    <div className="flex flex-col justify-between p-4 leading-normal">
-                      <a
-                        href={link}
-                        className="underline-offset-2 hover:underline"
-                      >
-                        <h5 className="text-xl font-semibold text-ghostindigo-900 dark:text-white">
-                          {company}
-                        </h5>
-                      </a>
-                      <h6 className="mb-2 text-sm text-ghostindigo-800 dark:text-ghostindigo-400">
-                        {title}
-                      </h6>
-                      <p className="mb-3 font-normal leading-7 text-ghostindigo-700 dark:text-gray-400">
-                        {description}
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              );
-            }
-          )}
-        </ul>
+        <ExperienceList work={work} />
       </div>
     </PageContainer>
   );
