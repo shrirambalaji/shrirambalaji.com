39e5c4b977be3de098d4be24d11365c5d5364ab7
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-26 20:58:31 +0530

    chore: remove unused /opengraph app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,6 @@ import { Layout } from "../components/Layout";
 import "../styles/globals.css";
 
 function App({ Component, pageProps, router }: AppProps) {
-  if (router.pathname.startsWith("/opengraph")) {
-    return (
-      <div className="bg-ghostindigo-900">
-        <Component {...pageProps}></Component>;
-      </div>
-    );
-  }
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
       <div className="bg-white dark:bg-ghostindigo-900">
