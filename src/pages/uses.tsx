a516af4e6913f70f4478cdd6e63a959910f49fd5
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-31 16:56:28 +0530

    feat(uses): add progressive image loading with blur effect

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -2,12 +2,19 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
 import { PageContainer } from "../components/PageContainer";
 import { readMDXFile } from "../util/mdx";
 import Image from "next/future/image";
+import { getPlaiceholder, IGetPlaiceholderReturn } from "plaiceholder";
 
-const components = {
-  Image,
-};
+interface ImageProps extends Partial<IGetPlaiceholderReturn> {
+  blurDataURL: string;
+}
 
-export default function Uses({ ...props }: MDXRemoteProps) {
+type UsesPageProps = MDXRemoteProps & { imageProps: ImageProps };
+export default function Uses(props: UsesPageProps) {
+  const { imageProps } = props;
+  const components = {
+    Image: (props: any) => <Image {...imageProps} {...props} />,
+  };
+
   return (
     <PageContainer>
       <div className="prose prose-slate prose-h1:text-[1.25rem] prose-h1:font-bold prose-h1:text-black prose-a:text-gray-600  prose-img:rounded-lg prose-img:border-2 prose-img:border-solid  prose-img:border-blue-500  prose-img:shadow-3xl prose-img:shadow-blue-400 dark:prose-invert dark:prose-h1:text-ghostindigo-300 dark:prose-a:text-indigo-300  dark:prose-img:border-blue-500   dark:prose-img:shadow-blue-900">
@@ -21,6 +28,15 @@ export default function Uses({ ...props }: MDXRemoteProps) {
 }
 
 export async function getStaticProps() {
+  const { base64, img } = await getPlaiceholder("/images/workstation.png");
   const { compiledSource } = await readMDXFile("src/content/uses.mdx");
-  return { props: { compiledSource } };
+  return {
+    props: {
+      compiledSource,
+      imageProps: {
+        ...img,
+        blurDataURL: base64,
+      },
+    },
+  };
 }
