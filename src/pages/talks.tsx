c4c2059f364983f4b4c502114442288e6b8b9867
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-31 15:52:26 +0530

    feat(talks): replace iframe with react-lite-youtube-embed in /talks
    * add border styles to each talk

--- src/pages/talks.tsx
+++ src/pages/talks.tsx
@@ -1,13 +1,22 @@
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
 import { PageContainer } from "../components/PageContainer";
 import { readMDXFile } from "../util/mdx";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+const components = {
+  YoutubeEmbed: LiteYouTubeEmbed,
+};
 
 // TODO: preload youtube embed iframe
 export default function Talks({ ...props }: MDXRemoteProps) {
   return (
     <PageContainer>
       <div className="prose prose-slate prose-h2:mb-1 prose-h2:font-bold prose-h2:leading-10 prose-h2:tracking-normal prose-h2:text-black prose-a:text-ghostindigo-400 dark:prose-invert  dark:prose-h2:text-indigo-200  dark:prose-a:text-indigo-300 prose-h2:md:mb-4 prose-h2:md:text-2xl">
-        <MDXRemote compiledSource={props.compiledSource} />
+        <MDXRemote
+          compiledSource={props.compiledSource}
+          components={components}
+        />
       </div>
     </PageContainer>
   );
--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -13,7 +13,6 @@ export default function Uses({ ...props }: MDXRemoteProps) {
       <div className="prose prose-slate prose-h1:text-[1.25rem] prose-h1:font-bold prose-h1:text-black prose-a:text-gray-600  prose-img:rounded-lg prose-img:border-2 prose-img:border-solid  prose-img:border-blue-500  prose-img:shadow-3xl prose-img:shadow-blue-400 dark:prose-invert dark:prose-h1:text-ghostindigo-300 dark:prose-a:text-indigo-300  dark:prose-img:border-blue-500   dark:prose-img:shadow-blue-900">
         <MDXRemote
           compiledSource={props.compiledSource}
-          // @ts-ignore
           components={components}
         />
       </div>
