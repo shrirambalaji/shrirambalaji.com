19dc0d3ae8200bdce2c6a2472411a0e08f68afe8
Shriram Balaji <hello@shrirambalaji.dev>
2022-09-20 01:40:57 +0530

    style: update PageContainer padding

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const PageContainer = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <main className="mx-auto my-2 flex max-w-3xl flex-col justify-center gap-10 p-0 pb-4 md:pb-8">
+    <main className="mx-auto my-2 flex max-w-3xl flex-col justify-center gap-10 p-0 pb-4">
       {children}
     </main>
   );
