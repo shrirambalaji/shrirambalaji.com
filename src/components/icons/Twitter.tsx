de82ccecfe907c6cfe51039248c47e5ed7e1ca87
Shriram Balaji <hello@shrirambalaji.com>
2025-06-18 16:50:44 +0530

    update social link icons

--- src/components/icons/Twitter.tsx
+++ src/components/icons/Twitter.tsx
@@ -11,14 +11,14 @@ export const Twitter = (
         role="link"
         aria-label="Shriram's Twitter profile"
         className={cn(
-          "group inline-flex cursor-pointer items-center rounded-lg p-2 px-3 text-center text-current",
+          "group inline-flex cursor-pointer items-center rounded-lg px-3 text-center text-current",
           props.className
         )}
         tabIndex={0}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 24 24" fill="currentColor">
           <path
-            d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"
+            d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
           />
         </svg>
       </span>
