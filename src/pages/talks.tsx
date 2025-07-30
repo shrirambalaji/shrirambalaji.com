2b131c5a66b544df844fed5df7a553d9ca536425
Shriram Balaji <hello@shrirambalaji.com>
2025-07-31 02:27:14 +0530

    fix: youtube embed in node 20 with next

--- src/pages/talks.tsx
+++ src/pages/talks.tsx
@@ -1,8 +1,12 @@
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
 import { PageContainer } from "../components/PageContainer";
 import { readMDXFile } from "../util/mdx";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import dynamic from "next/dynamic";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+const LiteYouTubeEmbed = dynamic(() => import("react-lite-youtube-embed"), {
+  ssr: false,
+});
 
 const components = {
   YoutubeEmbed: LiteYouTubeEmbed,
@@ -11,9 +15,9 @@ const components = {
 export default function Talks({ ...props }: MDXRemoteProps) {
   return (
     <PageContainer>
-      <div className="prose prose-slate prose-h2:mb-1  prose-h2:text-xl prose-h2:tracking-wide prose-h2:font-bold prose-h2:leading-10 prose-h3:text-ghostindigo-800 prose-a:text-ghostindigo-400 dark:prose-invert  dark:prose-h3:text-indigo-200  dark:prose-a:text-indigo-300 prose-h2:md:mb-4 prose-h2:md:text-xl prose-h3:md:text-lg">
+      <div className="prose prose-slate prose-h1:text-xl prose-h1:font-semibold prose-h1:font-sans prose-h1:tracking-normal prose-h1:text-ghostindigo-800 prose-h2:mb-1  prose-h2:text-xl prose-h2:tracking-normal prose-h2:font-semibold prose-h2:leading-10 prose-h3:text-ghostindigo-800 prose-a:text-ghostindigo-400 dark:prose-invert dark:prose-h1:text-white dark:prose-h3:text-indigo-200  dark:prose-a:text-indigo-300 prose-h2:md:mb-4 prose-h2:md:text-xl prose-h3:md:text-lg">
         <MDXRemote
-          compiledSource={props.compiledSource}
+          {...props}
           components={components}
         />
       </div>
@@ -22,6 +26,6 @@ export default function Talks({ ...props }: MDXRemoteProps) {
 }
 
 export async function getStaticProps() {
-  const { compiledSource } = await readMDXFile("src/content/talks.mdx");
-  return { props: { compiledSource } };
+  const mdxSource = await readMDXFile("src/content/talks.mdx");
+  return { props: mdxSource };
 }
